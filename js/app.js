031af9e5a9b375ac8441df89236e813a44ecfcb9
aaltamirano1 <angel.g.altam@gmail.com>
2018-05-06 17:16:23 -0400

    add comments to js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+// box at the top of the page with the symbol O or X is highlighted for the current player.
 function toggleActivePlayer(){
 	$('#player1').toggleClass('active');
 	$('#player2').toggleClass('active');
 }
+// game ends when one player has three of their symbols in a row horizontally/vertically/diagonally.
 function findWinner(){
 	rowWins('x');
 	rowWins('o');
@@ -11,6 +13,7 @@ function findWinner(){
 	diagonalwin1('o');
 	diagonalwin2('x');
 	diagonalwin2('o');
+	// if all squares are filled and no players have three in a row, the game is a tie.
 	tie();
 }
 var b = $('.box');
@@ -69,6 +72,7 @@ function o(box){
 function x(box){
 	return box.classList.contains('box-filled-2');
 }
+// when the game ends, the board disappears and the game end screen appears.
 function win(player){
 	$('body').append(`
 		<div class="screen screen-win" id="finish">
@@ -79,14 +83,17 @@ function win(player){
 		  </header>
 		</div>
 	`);
+	// add the appropriate class (and therefore background) to the <div> for the winning screen.
 	if (player==='o') {
 		$('.screen').addClass('screen-win-one');
 	}else if (player==='x') {
 		$('.screen').addClass('screen-win-two');
+	// if the game ends in a tie, show the phrase "It's a Tie!" instead of "Winner".
 	}else if (player==='tie') {
 		$('.screen').addClass('screen-win-tie');
-		$('.message').text("It's a draw");
+		$('.message').text("It's a Tie!");
 	}
+	// when a player clicks "New Game" button, the board appears again, empty, and a new game begins.
 	$('#finish a').click(function(){
 		$('#finish').remove();
 		$('#player1').addClass('active');
@@ -108,6 +115,7 @@ function tie(){
 }
 
 $(document).ready(function() {
+	// when the page loads, the startup screen should appear. 
 	$('body').append(`
 		<div class="screen screen-start" id="start">
 		  <header>
@@ -116,13 +124,17 @@ $(document).ready(function() {
 		  </header>
 		</div>
 	`);
+	// when the player clicks start button start screen disappears, board appears, game begins.
 	$('#start a').click(function(){
 		$('#start').hide();
 		$('#player1').addClass('active');
 	});
 	$('.box').click(function(){
+		// players can only click on empty squares.
 		if(!$(this).hasClass('box-filled-1') && !$(this).hasClass('box-filled-2')){
+			// play alternates between X and O.
 			toggleActivePlayer();
+			// when the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square.
 			if ($('#player1').hasClass('active')){
 				$(this).addClass('box-filled-2');
 			} else if ($('#player2').hasClass('active')){
